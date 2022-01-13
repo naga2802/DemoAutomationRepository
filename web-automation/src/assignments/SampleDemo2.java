1a04cbd8b3d85dd7dd784b55cf8081ce7abbfa64
naga2802 <naga.s2802@gmail.com>
2022-01-13 20:17:09 +0530

    table

---
+++ web-automation/src/assignments/SampleDemo2.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class SampleDemo2 {
+
+	public static void main(String[] args) {
+		  for (int i=1;i<=10;i++)
+			 {
+			 System.out.println("10*"+i+"=" +(10*i));
+			 }
+
+	}
+
+}
